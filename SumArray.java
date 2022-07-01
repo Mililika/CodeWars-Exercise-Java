6d8389060a9b03a34b778c3bac526c8bc6c28046
mykhailokazymyr <michail.kazimir@gmail.com>
2022-07-01 20:28:51 +0200

    Task 1 | Sum of all elements in array

--- SumArray.java
+++ SumArray.java
@@ -1,9 +1,7 @@
 public class SumArray {
     public static double sum(double[] numbers) {
         double resultSum = 0;
-        if (numbers.length == 0) {
-            return 0;
-        }
+        if (numbers.length == 0) { return 0; }
 
         for (double elem : numbers) {
             resultSum += elem;
