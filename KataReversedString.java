05d836d59804069e30106ef99211af63bb56ab09
mykhailokazymyr <michail.kazimir@gmail.com>
2022-07-01 20:36:21 +0200

    Task 5 | Reverse String

---
+++ KataReversedString.java
@@ -0,0 +1,7 @@
+public class KataReversedString {
+    public static String solution(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+}
